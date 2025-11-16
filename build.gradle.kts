f0351ab08a8e60d99cc643f42976f91e48f28cf3
hashfunc <root@hashfunc.io>
2025-11-16 17:04:39 +0900

    chore: cleanup dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,5 @@
-import org.springframework.boot.gradle.tasks.bundling.BootJar
-
 plugins {
     kotlin("jvm") version "1.9.25"
-    kotlin("plugin.spring") version "1.9.25"
-    kotlin("plugin.jpa") version "1.9.25"
-
-    id("org.springframework.boot") version "3.5.7"
-    id("io.spring.dependency-management") version "1.1.7"
-    id("com.diffplug.spotless") version "8.0.0"
 }
 
 allprojects {
@@ -16,8 +8,6 @@ allprojects {
     description = "Demo project for Spring Boot"
 
     apply(plugin = "java")
-    apply(plugin = "org.springframework.boot")
-	apply(plugin = "io.spring.dependency-management")
 
     java {
         toolchain {
@@ -27,44 +17,5 @@ allprojects {
 
     repositories {
         mavenCentral()
-    }
-
-    tasks.getByName<BootJar>("bootJar") {
-        enabled = false
-    }
-}
-
-subprojects {
-    apply(plugin = "kotlin")
-    apply(plugin = "kotlin-spring")
-    apply(plugin = "kotlin-kapt")
-
-    dependencies {
-        implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-        implementation("org.springframework.boot:spring-boot-starter-web")
-        implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-        implementation("org.jetbrains.kotlin:kotlin-reflect")
-        runtimeOnly("org.postgresql:postgresql")
-        implementation("io.lettuce:lettuce-core")
-
-        testImplementation("org.springframework.boot:spring-boot-starter-test")
-        testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-        testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-    }
-
-    kotlin {
-        compilerOptions {
-            freeCompilerArgs.addAll("-Xjsr305=strict")
-        }
-    }
-
-    allOpen {
-        annotation("jakarta.persistence.Entity")
-        annotation("jakarta.persistence.MappedSuperclass")
-        annotation("jakarta.persistence.Embeddable")
-    }
-
-    tasks.withType<Test> {
-        useJUnitPlatform()
     }
 }
